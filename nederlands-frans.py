66b66e8efc074d77a0244d794dd82125bde11951
XxedenhuxX <edenhu@leerling.wico.be>
2025-11-15 19:45:00 +0100

    foute antw opnieuw vrage

--- nederlands-frans.py
+++ nederlands-frans.py
@@ -6,33 +6,38 @@ woorden={'ik':'je',
          ,'hij':'il'}
 
 
-fouteantw=list(woorden)
+
 def frans_naar_nederlands():
     aselect= list(woorden.keys())
     random.shuffle(aselect)
-    for nlwoord in aselect:
-        vraag = input(f"wat betekend {woorden[nlwoord]}?")
-        if vraag ==nlwoord:
-           print("goed gedaan")
-           fouteantw.remove(nlwoord)
-        else:
-            print("L")
-    print(fouteantw)
+    fouteantw = list(woorden.keys())
+    while fouteantw:
+        for nlwoord in aselect:
+            vraag = input(f"wat betekend {woorden[nlwoord]}?\n")
+            if vraag ==nlwoord:
+               print("goed gedaan")
+               fouteantw.remove(nlwoord)
+            else:
+                print("L")
+        print(fouteantw)
 
 
 
 def nederland_naar_frans():
     aselect=list(woorden.keys())
     random.shuffle(aselect)
-    for nlwoord in aselect:
-        frwoord=input(f"wat betekent {nlwoord}?")
-        if frwoord==woorden[nlwoord]:
-            print("goed gedaan")
-            fouteantw.remove(nlwoord)
-        else: print("L")
-    print(fouteantw)
+    fouteantw = list(woorden.keys())
+    while fouteantw:
+        for nlwoord in aselect:
+            frwoord=input(f"wat betekent {nlwoord}?\n")
+            if frwoord==woorden[nlwoord]:
+                print("goed gedaan")
+                fouteantw.remove(nlwoord)
+            else: print("L")
+
+        print(fouteantw)
 def vraag():
-    antw=input('type frans voor frans naar nederlands en nederlands voor nederlands naar frans')
+    antw=input('type frans voor frans naar nederlands en nederlands voor nederlands naar frans\n')
     if antw == "frans":
             return frans_naar_nederlands()
     elif antw == "nederlands":
